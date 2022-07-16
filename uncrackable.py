e2655373ed98d4fa6d7b5388154b32b262fdb4e3
Deleon <jerry_howl_lawliet@hotmail.com>
2022-07-15 20:41:10 -0500

    Update uncrackable.py

--- uncrackable.py
+++ uncrackable.py
@@ -1,6 +1,6 @@
-# Tested in: Python 3.8.8 - Windows
+# Tested in: Python 3.8.8
 # By: LawlietJH
-# Uncrackable v1.0
+# Uncrackable v1.0.0
 # Descripción: Generador de contraseñas indecifrables a partir de una contraseña fácil de recordar
 #              Es más simple recordar una contraseña y con ello generar Contraseñas Fuertes.
 #              Con los mismos parametros, siempre generaremos las mismas palabras.
@@ -15,9 +15,13 @@
 import string
 import json
 
+#=======================================================================
+__author__  = 'LawlietJH'          # Desarrollador
+__title__   = 'Uncrackable'        # Título
+__version__ = 'v1.0.0'             # Versión
+#=======================================================================
 
-
-class Splitmix64:	# Pseudo-random number generator. From: https://github.com/LawlietJH/Utils
+class Splitmix64:	# Pseudo-random number generator (PRNG). From: https://github.com/LawlietJH/Utils
 	
 	def __init__(self, seed=0):
 		
@@ -269,54 +273,3 @@ def getUncrackablePasswords(passwd, qty=1, length=(20, 32),
 		out = json.dumps(out, indent=4)
 	
 	return out
-
-
-
-if __name__ == '__main__':
-	
-	# Ejemplos de Uso:
-	uncrkPwd = getUncrackablePasswords
-	
-	passwd = uncrkPwd('contraseñaFacil123', out_json=True)
-	print(passwd)
-	pwd_dict = json.loads(passwd)
-	print('Pwd-01 len:', len(pwd_dict['pwd-01']))
-	# Output: Obtenemos 1 contraseña con la palabara "ContraseñaFacil123"
-	# {
-	#   "pwd-01": "{Mi&?h2~0ccWPWVj.g#.yn"
-	# }
-	# Pwd-01 len: 22
-	
-	passwd = uncrkPwd('contraseñaFacil123', 3, out_json=True)
-	print(passwd)
-	# Output: Obtenemos 3 palabras con longitudes entre 20 y 32 por defecto, todas a raiz de la misma contraseña.
-	# {
-	#   "pwd-01": "{Mi&?h2~0ccWPWVj.g#.yn",
-	#   "pwd-02": "=?a#BY}A6oyMG}}KKd4IJ?a*y2QD?",
-	#   "pwd-03": "&JYEn.jQ{im$GnnbbAHe4EBVL"
-	# }
-	
-	passwd = uncrkPwd('contraseñaFacil123', qty=3, length=24, out_json=True)
-	print(passwd)
-	# Output: Obtenemos 3 palabras con longitud 24, todas a raiz de la misma contraseña.
-	# {
-	#   "pwd-01": "={Mi&?h2~0ccWPWVj.g#.ynk",
-	#   "pwd-02": "=?a#BY}A6oyMG}}KKd4IJ?a*",
-	#   "pwd-03": "y2QD?N&JYEn.jQ{im$GnnbbA"
-	# }
-	
-	passwd = uncrkPwd('contraseñaFacil123', punc=False, length=32, out_json=True)
-	print(passwd)
-	# Es posible quitar: lower=False, upper=False, digits=False
-	# y añadir caracteres extras con charset='¡¿¬' por ejemplo.
-	# Output: Obtenemos una contraseña de longitud 32 pero sin signos de puntuación:
-	# {
-	#   "pwd-01": "FxQj3Gj9z8eeYTYXk6h16wnkFGd1IZyH"
-	# }
-	
-	passwd = uncrkPwd('contraseña facil 123', only='0123456789ABCDEF', length=32, out_json=True)
-	print(passwd)
-	# Output: Obtenemos una contraseña de longitud 32 pero solamente con los caracteres dados:
-	# {
-	#   "pwd-01": "419093CDE66CC9C936558216BB85B7C2"
-	# }
